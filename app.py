d019d7067ddbd44b16634816fcd7e8aead72d530
Nithish699 <nithish699734@gmail.com>
2025-04-27 13:01:59 +0530

    Fix import redundancy by removing duplicate joblib import statement.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import matplotlib.pyplot as plt
-import joblib
+
 
 #==============================================================================================|
 
